6218d58ac2d11ef91a15af02d53e17367a716f15
CIPHERTron <pritish.samal918@gmail.com>
2021-08-15 14:04:05 +0530

    Primitives | Strings & Runes

--- data-types.go
+++ data-types.go
@@ -42,4 +42,16 @@ func main() {
 	fmt.Println(c * d)
 	fmt.Println(c / d)
 
+	// Text type
+	// 1. String
+	s := "Hello World!"
+	fmt.Printf("%v, %T\n", string(s[2]), string(s[2])) // 108, uint8
+
+	s1 := "this is a string"
+	s2 := []byte(s1)
+	fmt.Printf("%v, %T\n", s2, s2) // [116 104 105 115 32 105 115 32 97 32 115 116 114 105 110 103], []uint8
+
+	// Runes
+	var r rune = 'a'
+	fmt.Printf("%v, %T\n", r, r) // 97, int32
 }
